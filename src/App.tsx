418f9e6cc629124687c7e47da538e779e2080c93
nastya-nealova <nastya20001492@gmail.com>
2023-11-28 15:50:55 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import '@pqina/pintura/pintura.css';
+import { getEditorDefaults } from '@pqina/pintura';
+import { PinturaEditor } from '@pqina/react-pintura';
+
+const editorConfig = getEditorDefaults();
+
+function App() {
+    return (
+        <div className="App" style={{ height: '600px' }}>
+            <PinturaEditor
+                {...editorConfig}
+                src="mercedes.jpg"
+                imageCropAspectRatio={1}
+            ></PinturaEditor>
+        </div>
+    );
+}
+export default App;
